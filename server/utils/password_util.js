3eb45e39b8135b6797b2a3504367680c37f1d98c
Minggoy <jerkboy000@gmail.com>
2023-12-27 13:21:27 +0800

    SERVER: Created utilities for password and token.

---
+++ server/utils/password_util.js
@@ -0,0 +1,33 @@
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const logger = require("../src/logger");
+
+// Function to hash (encrypt) a password
+const hashPassword = async (plainPassword) => {
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(plainPassword, salt);
+    logger.info(`Password hashed successfully`);
+    return hashedPassword;
+  } catch (error) {
+    logger.error(`Error hashing password: ${error.message}`);
+    throw error;
+  }
+};
+
+// Function to verify (decrypt) a password
+const verifyPassword = async (plainPassword, hashedPassword) => {
+  try {
+    const isMatch = await bcrypt.compare(plainPassword, hashedPassword);
+    logger.info(`Password verified successfully`);
+    return isMatch;
+  } catch (error) {
+    logger.error(`Error verifying password: ${error.message}`);
+    throw error;
+  }
+};
+
+module.exports = {
+  hashPassword,
+  verifyPassword,
+};
---
+++ server/utils/token_util.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const logger = require("../src/logger");
+
+// Function that generates token
+const generateToken = (user) => {
+  try {
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.AUTH_SECRET_KEY,
+      { expiresIn: '1h' }
+    );
+    logger.info(`Token generated successfully for user: ${user.email}`);
+    return token;
+  } catch (error) {
+    logger.error(`Error generating token: ${error.message}`);
+    throw error;
+  }
+};
+
+// Function that verifies token
+const verifyToken = (token) => {
+  try {
+    const decodedToken = jwt.verify(token, process.env.AUTH_SECRET_KEY);
+    logger.info(`Token verified successfully`);
+    return decodedToken;
+  } catch (error) {
+    logger.error(`Error verifying token: ${error.message}`);
+    return null;
+  }
+};
+
+module.exports = { 
+    generateToken,
+    verifyToken,
+};
