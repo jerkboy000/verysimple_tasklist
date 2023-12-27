eb94ad4e152142adb1cb5d7c81c4d1429de5fbd5
Minggoy <jerkboy000@gmail.com>
2023-12-27 13:20:11 +0800

    SERVER: Configuration for database and logger.

---
+++ server/src/logger.js
@@ -0,0 +1,28 @@
+const  { createLogger, format, transports } = require("winston");
+const path = require('path');
+
+const logDirectory = path.join(__dirname, "../logs");
+
+const logFormat = format.combine(
+  format.timestamp(),
+  format.simple()
+);
+
+const logger = createLogger({
+  level: "info",
+  format: logFormat,
+  transports: [
+    new transports.Console({ 
+        format: format.simple() 
+    }),
+    new transports.File({ 
+        filename: path.join(logDirectory, "error.log"),
+        level: "error" 
+    }),
+    new transports.File({ 
+        filename: path.join(logDirectory, "combined.log") 
+    })
+  ]
+});
+
+module.exports = logger;
