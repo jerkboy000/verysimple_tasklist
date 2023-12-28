1a76a079ff3edf3df1651eaae891720eb57530a0
Minggoy <jerkboy000@gmail.com>
2023-12-28 10:58:01 +0800

    CLIENT: Added footer content.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer className="sticky-footer">
-        <br />
+    <footer className="mt-5 py-3 bg-dark text-light text-center">
+      <p>
+        &copy; {currentYear} Very Simple Task App | Created by Minggoy
+      </p>
     </footer>
   );
 };
