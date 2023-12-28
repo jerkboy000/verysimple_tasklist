b99be1ea5bab28fee9028342c9e467fba629dea7
Minggoy <jerkboy000@gmail.com>
2023-12-28 10:20:06 +0800

    CLIENT: Applied bootstrap to sign up and login.

--- client/src/components/forms/user/LogInForm.jsx
+++ client/src/components/forms/user/LogInForm.jsx
@@ -3,11 +3,10 @@ import { useNavigate } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import { LOGIN_MUTATION } from "../../../graphql/mutations";
 import { setAuthCookies } from "../../js/cookie_util";
-import "../../css/FormStyles.css";
 
 const LogInForm = () => {
   const navigate = useNavigate();
-  const [formData, setFormData] = useState({ email: '', password: '' });
+  const [formData, setFormData] = useState({ email: "", password: "" });
   const [loginMutation, { loading, error }] = useMutation(LOGIN_MUTATION);
 
   const handleChange = (e) => {
@@ -24,55 +23,67 @@ const LogInForm = () => {
 
       const loggedUser = response.data.loginUser;
       setAuthCookies(loggedUser);
-      console.log('Logged in successfully:', response.data.loginUser);
+      console.log("Logged in successfully:", response.data.loginUser);
 
       // Redirect to task list
       navigate("/tasklist");
       window.location.reload();
     } catch (loginError) {
-      console.error('Error logging in:', loginError.message);
+      console.error("Error logging in:", loginError.message);
     }
   };
 
   return (
-    <div className="form-container">
-      <form onSubmit={handleLogin}>
-        <h2>Log In</h2>
+    <div className="container mt-5 d-flex justify-content-center">
+      <form
+        onSubmit={handleLogin}
+        className="p-4 rounded"
+        style={{ backgroundColor: "#e0f7fa" }} // Very faint sky blue
+      >
+        <h2 className="mb-4">Log In</h2>
 
-        <div>
-          <label htmlFor="email">Email:</label>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email:
+          </label>
           <input
             type="email"
             id="email"
             name="email"
             value={formData.email}
             onChange={handleChange}
-            className="form-input"
+            className="form-control"
           />
         </div>
 
-        <div>
-          <label htmlFor="password">Password:</label>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password:
+          </label>
           <input
             type="password"
             id="password"
             name="password"
             value={formData.password}
             onChange={handleChange}
-            className="form-input"
+            className="form-control"
           />
         </div>
 
-        <div>
-          <button type="submit" disabled={loading} className="form-button">
+        <div className="mb-3 d-flex justify-content-center">
+          <button type="submit" disabled={loading} className="btn btn-success me-2">
             Log In
           </button>
-          <button type="button" className="form-button cancel-button" onClick={() => navigate("/")}>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => navigate("/")}
+          >
             Cancel
           </button>
         </div>
 
-        {error && <p className="form-error">{error.message}</p>}
+        {error && <p className="text-danger">{error.message}</p>}
       </form>
     </div>
   );
--- client/src/components/forms/user/SignUpForm.jsx
+++ client/src/components/forms/user/SignUpForm.jsx
@@ -3,11 +3,15 @@ import { useNavigate } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import { SIGNUP_MUTATION } from "../../../graphql/mutations";
 import { setAuthCookies } from "../../js/cookie_util";
-import "../../css/FormStyles.css";
 
 const SignUpForm = () => {
   const navigate = useNavigate();
-  const [formData, setFormData] = useState({ name: '', email: '', password: '', confirmPassword: '' });
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
   const [signupMutation, { loading, error }] = useMutation(SIGNUP_MUTATION);
 
   const handleChange = (e) => {
@@ -18,7 +22,7 @@ const SignUpForm = () => {
     e.preventDefault();
 
     if (formData.password !== formData.confirmPassword) {
-      console.error('Password and confirmation do not match');
+      console.error("Password and confirmation do not match");
       return;
     }
 
@@ -30,78 +34,94 @@ const SignUpForm = () => {
       const signedupUser = response.data.signupUser;
       setAuthCookies(signedupUser);
 
-      console.log('Signed up successfully:', response.data.signupUser);
+      console.log("Signed up successfully:", response.data.signupUser);
       // Redirect to task list
       navigate("/tasklist");
       window.location.reload();
     } catch (signupError) {
-      console.error('Error signing up:', signupError.message);
+      console.error("Error signing up:", signupError.message);
     }
   };
 
   return (
-    <div className="form-container">
-      <form onSubmit={handleSignUp}>
-        <h2>Sign Up</h2>
+    <div className="container mt-5 d-flex justify-content-center">
+      <form
+        onSubmit={handleSignUp}
+        className="p-4 rounded"
+        style={{ backgroundColor: "#e0f7fa" }} // Very faint sky blue
+      >
+        <h2 className="mb-4">Sign Up</h2>
 
-        <div>
-          <label htmlFor="name">Name:</label>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name:
+          </label>
           <input
             type="text"
             id="name"
             name="name"
             value={formData.name}
             onChange={handleChange}
-            className="form-input"
+            className="form-control"
           />
         </div>
 
-        <div>
-          <label htmlFor="email">Email:</label>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email:
+          </label>
           <input
             type="email"
             id="email"
             name="email"
             value={formData.email}
             onChange={handleChange}
-            className="form-input"
+            className="form-control"
           />
         </div>
 
-        <div>
-          <label htmlFor="password">Password:</label>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password:
+          </label>
           <input
             type="password"
             id="password"
             name="password"
             value={formData.password}
             onChange={handleChange}
-            className="form-input"
+            className="form-control"
           />
         </div>
 
-        <div>
-          <label htmlFor="confirmPassword">Confirm Password:</label>
+        <div className="mb-3">
+          <label htmlFor="confirmPassword" className="form-label">
+            Confirm Password:
+          </label>
           <input
             type="password"
             id="confirmPassword"
             name="confirmPassword"
             value={formData.confirmPassword}
             onChange={handleChange}
-            className="form-input"
+            className="form-control"
           />
         </div>
 
-        <div>
-          <button type="submit" disabled={loading} className="form-button">
+        <div className="mb-3 d-flex justify-content-center">
+          <button type="submit" disabled={loading} className="btn btn-success me-2">
             Sign Up
           </button>
-          <button type="button" className="form-button cancel-button" onClick={() => navigate("/")}>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => navigate("/")}
+          >
             Cancel
           </button>
         </div>
 
-        {error && <p className="form-error">{error.message}</p>}
+        {error && <p className="text-danger">{error.message}</p>}
       </form>
     </div>
   );
