7c3eebc50b614cecd3bc94e00b09032de7c36b84
Minggoy <jerkboy000@gmail.com>
2023-12-28 12:09:20 +0800

    CLIENT: Improved the content of Home Page. WIP.

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -26,17 +26,50 @@ const HomePage = () => {
   };
 
   return (
-     <div>
-      {isLoggedIn ? (
-        <center>
-          <h1>You are logged in!</h1>
-        </center>
-      ) : (
-        <>
-          <button onClick={handleLoginClick}>Login</button>
-          <button onClick={handleSignUpClick}>Sign Up</button>
-        </>
-      )}
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-lg-6">
+          <h1>Welcome to Very Simple Task List App</h1>
+          <p className="lead">
+            Organize your tasks effortlessly with our user-friendly task
+            management application. Sign up or log in to get started!
+          </p>
+          {!isLoggedIn && (
+            <div className="d-grid gap-2">
+              <button
+                className="btn btn-primary btn-lg"
+                onClick={handleSignUpClick}
+              >
+                Sign Up
+              </button>
+              <button
+                className="btn btn-outline-primary btn-lg"
+                onClick={handleLoginClick}
+              >
+                Log In
+              </button>
+            </div>
+          )}
+        </div>
+        {isLoggedIn && (
+          <div className="col-lg-6">
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-title">You are logged in!</h2>
+                <p className="card-text">
+                  Explore your tasks and start managing them efficiently.
+                </p>
+                <button
+                  className="btn btn-primary"
+                  onClick={() => navigate("/tasklist")}
+                >
+                  View Tasks
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
